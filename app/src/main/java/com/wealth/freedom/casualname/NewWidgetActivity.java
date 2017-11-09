11f1b249e4db6d18b2f79e433892c3de4cd4d48f
captain <714226230@qq.com>
2017-11-09 11:32:12 +0800

    TextInput的使用

---
+++ app/src/main/java/com/wealth/freedom/casualname/NewWidgetActivity.java
@@ -0,0 +1,26 @@
+package com.wealth.freedom.casualname;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.wealth.freedom.newwidget.TextInputLayoutActivity;
+
+/**
+ * Author:  captain
+ * Time:  2017/11/9 0009
+ * Describe:
+ */
+public class NewWidgetActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_widget);
+    }
+
+    public void inputLayout(View view){
+        startActivity(new Intent(this, TextInputLayoutActivity.class));
+    }
+}
